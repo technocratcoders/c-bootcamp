c858cd870673362b3549230bf57946eea926c2ff
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-25 20:28:15 +0530

    Update Day-03 (04).c

--- Solutions/Day 03/Day-03 (04).c
+++ Solutions/Day 03/Day-03 (04).c
@@ -1,19 +1,15 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-03
+
+Q.4) Write a program to print your name.
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
-    }
-    
-    printf("%d", ans);
+    printf("Technocrat Coders");         // Printing the name
     
     return 0;
 }
