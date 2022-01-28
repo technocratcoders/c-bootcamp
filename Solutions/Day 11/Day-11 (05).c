846c5bfe00912f79abcb1ad8e3144fe8e426d4f7
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-28 21:57:31 +0530

    Update Day-11 (05).c

--- Solutions/Day 11/Day-11 (05).c
+++ Solutions/Day 11/Day-11 (05).c
@@ -1,5 +1,8 @@
 /*
-Q) Write a program to print the following pattern -
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-11
+
+Q.5) Write a program to print the following pattern -
     *
    * *
   * * *
@@ -17,28 +20,38 @@ int main() {
     int n;
     printf("Enter the value of n: ");
     scanf("%d", &n);
-
-    int size = 2*n - 1;
-
-    for(int i=0; i<size; i++) {
-        // Printing white spaces
-        if((i+1) != n) {
-            int space = n - i;
-            while(space) {
-                printf(" ");
-                space--;
-            }
-        }
-
-        // Getting the number of stars to be printed in the row.
-        int star;
-        i>=n ? star = size-i : star = i;
-
+    
+    int space;
+    
+    // Printing downward's triangle
+    space = n - 1;
+    for (int j=1; j<=n; j++)
+    {
+        // Printing spaces
+        for(int k=1; k<=space; k++)
+        printf("-");
+        
         // Printing stars
-        for(int j=0; j<star; j++) {
-            printf("* ");
-        }
-
+        for(int k=1; k<=2*j-1; k++)
+        printf("*");
+        
+        space--;
+        printf("\n");
+    }    
+    
+    // Printing downward's triangle
+    space = 1;
+    for (int j = 1; j <= n - 1; j++)
+    {
+        // Printing spaces
+        for (int k=1; k<=space; k++)
+        printf("-");
+        
+        // Printing stars
+        for (int k=1 ; k<=2*(n-j)-1; k++)
+        printf("*");
+        
+        space++;
         printf("\n");
     }
     
