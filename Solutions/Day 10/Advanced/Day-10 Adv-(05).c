ab602c519b2bfd24723970120b8c16e50c3e26fd
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-29 11:14:42 +0530

    Update Day-10 Adv-(05).c

--- Solutions/Day 10/Advanced/Day-10 Adv-(05).c
+++ Solutions/Day 10/Advanced/Day-10 Adv-(05).c
@@ -1,19 +1,34 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-10 (Advanced)
+
+Q.5) Write a program to print the following pattern -
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
+    int count = 1;
+    
+    for(int i=1; i<=n; i++) {
+        
+        // Printing numbers
+        for(int j=1; j<=i; j++) {
+            printf("%d ", count);
+            count++;
+        }
+        
+        printf("\n");
     }
-    
-    printf("%d", ans);
     
     return 0;
 }
