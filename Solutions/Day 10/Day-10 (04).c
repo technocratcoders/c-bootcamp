4c0cf7381ebd5aca3701f53f653e21569b2d66f8
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-29 10:48:04 +0530

    Update Day-10 (04).c

--- Solutions/Day 10/Day-10 (04).c
+++ Solutions/Day 10/Day-10 (04).c
@@ -1,19 +1,29 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-10
+
+Q.4) Write a program to calculate the power of a given number upto a given term.
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
+    int num, term;
+    printf("Enter the number: ");
     scanf("%d", &num);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
+    printf("Enter the term: ");
+    scanf("%d", &term);
+    
+    int power = 1;
+    
+    for(int i=1; i<=term; i++) {
+        // Multiplying 'num' with itself 'term' times
+        power = power * num;
     }
     
-    printf("%d", ans);
+    // Printing power
+    printf("%d to the power %d is: %d", num, term, power);
     
     return 0;
 }
