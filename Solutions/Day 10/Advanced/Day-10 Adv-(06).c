99c93e43a5baef8fac78dadf4c17043e1c135a6c
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-29 11:20:26 +0530

    Update Day-10 Adv-(06).c

--- Solutions/Day 10/Advanced/Day-10 Adv-(06).c
+++ Solutions/Day 10/Advanced/Day-10 Adv-(06).c
@@ -1,19 +1,39 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-10 (Advanced)
+
+Q.6) Write a program to print the following pattern -
+        1
+      2 2 2
+    3 3 3 3 3
+  4 4 4 4 4 4 4
+5 5 5 5 5 5 5 5 5
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
+    for(int i=1; i<=n; i++) {
+        int space = n - i;
+        
+        // Printing spaces
+        for(int j=1; j<=space; j++) {
+            printf("  ");
+        }
+        
+        int count = 2*i - 1;
+        
+        // Printing numbers
+        for(int j=1; j<=count; j++) {
+            printf("%d ", i);
+        }
+        
+        printf("\n");
     }
-    
-    printf("%d", ans);
     
     return 0;
 }
