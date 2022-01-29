24fd008a77b2f56699e80bc06d6bc52aa781278a
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-29 11:28:48 +0530

    Update Day-10 Adv-(08).c

--- Solutions/Day 10/Advanced/Day-10 Adv-(08).c
+++ Solutions/Day 10/Advanced/Day-10 Adv-(08).c
@@ -1,19 +1,48 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-10 (Advanced)
+
+Q.8) Write a program to print the following pattern -
+        A
+      A B A
+    A B C B A
+  A B C D C B A
+A B C D E D C B A
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
+    for(int i=1; i<=n; i++) {
+        int space = n - i;
+        
+        // Printing spaces
+        for(int j=1; j<=space; j++) {
+            printf("  ");
+        }
+        
+        char ch = 'A';
+        
+        // Printing 1st triangle of numbers
+        for(int j=1; j<=i; j++) {
+            printf("%c ", ch);
+            ch++;
+        }
+        
+        ch = ch - 2;
+        
+        // Printing 2nd triangle of numbers
+        for(int j=1; j<i; j++) {
+            printf("%c ", ch);
+            ch--;
+        }
+        
+        printf("\n");
     }
-    
-    printf("%d", ans);
     
     return 0;
 }
