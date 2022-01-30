41676cc992777fce7aa84eaaf2b4fa06ed14dcca
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-30 22:10:58 +0530

    Update Day-09 (04).c

--- Solutions/Day 09/Day-09 (04).c
+++ Solutions/Day 09/Day-09 (04).c
@@ -1,19 +1,32 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-09
+
+Q.4) Write a program to check whether there is loss or profit by using cost price and selling price and also calculate its percentage.
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int cp, sp;
+    printf("Enter the cost price: ");
+    scanf("%d", &cp);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
-    }
+    printf("Enter the selling price: ");
+    scanf("%d", &sp);
     
-    printf("%d", ans);
+    if(sp > cp) {
+        
+        int profit = sp - cp;
+        printf("There is a profit of %d percentage", (profit * 100) / cp);
+            
+    } else if(cp > sp) {
+        
+        int loss = cp - sp;
+        printf("There is a loss of %d percentage", (loss * 100) / cp);
+        
+    } else
+        printf("There is 0 loss or profit");
     
     return 0;
 }
