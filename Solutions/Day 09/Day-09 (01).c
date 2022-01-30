c756f96972874e9e761dc144888ce96a6d2b359f
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-30 21:59:41 +0530

    Update Day-09 (01).c

--- Solutions/Day 09/Day-09 (01).c
+++ Solutions/Day 09/Day-09 (01).c
@@ -13,15 +13,19 @@ int main() {
     scanf("%d %d %d", &num1, &num2, &num3);
     
     if(num1 < num2) {
+        
         if(num1 < num3)
             printf("%d is the smallest", num1);
         else
             printf("%d is the smallest", num3);
+        
     } else {
+        
         if(num2 < num3)
             printf("%d is the smallest", num2);
         else
             printf("%d is the smallest", num3);
+        
     }
     
     return 0;
