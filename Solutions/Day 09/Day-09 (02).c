526f237459983e1242524a947748f60bb451cd46
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-30 21:47:48 +0530

    Update Day-09 (02).c

--- Solutions/Day 09/Day-09 (02).c
+++ Solutions/Day 09/Day-09 (02).c
@@ -1,19 +1,24 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-09
+
+Q.2) Write a program to check whether a number is divisible by 5 and 7 or not.
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     int num;
+    printf("Enter the number: ");
     scanf("%d", &num);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
-    }
-    
-    printf("%d", ans);
+    if(num % 5 == 0) {
+        if(num % 7 == 0)
+            printf("%d is divisible by both 5 and 7", num);
+        else
+            printf("%d is not divisible by both 5 and 7", num);
+    } else
+        printf("%d is not divisible by both 5 and 7", num);
     
     return 0;
 }
