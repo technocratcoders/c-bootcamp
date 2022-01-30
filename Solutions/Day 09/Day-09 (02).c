366fa1ba06ba67d6346dac36bc676717dc76b982
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-30 21:59:01 +0530

    Update Day-09 (02).c

--- Solutions/Day 09/Day-09 (02).c
+++ Solutions/Day 09/Day-09 (02).c
@@ -13,10 +13,12 @@ int main() {
     scanf("%d", &num);
     
     if(num % 5 == 0) {
+        
         if(num % 7 == 0)
             printf("%d is divisible by both 5 and 7", num);
         else
             printf("%d is not divisible by both 5 and 7", num);
+        
     } else
         printf("%d is not divisible by both 5 and 7", num);
     
