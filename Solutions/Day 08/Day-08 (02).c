bfeffe2bb9718681551a8a01ad04b1c6997f2add
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-30 21:20:14 +0530

    Update Day-08 (02).c

--- Solutions/Day 08/Day-08 (02).c
+++ Solutions/Day 08/Day-08 (02).c
@@ -1,19 +1,25 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-08
+
+Q.2) Write a program to find whether the entered number is divisible
+by 5 or not.
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
     int num;
+    printf("Enter a number: ");
     scanf("%d", &num);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
-    }
+    // Checking whether num is divisible by 5 or not
+    if(num % 5 == 0)
+        printf("%d is divisible by 5", num);
     
-    printf("%d", ans);
+    // Checking whether num is divisible by 5 or not
+    if(num % 5 != 0)
+        printf("%d is not divisible by 5", num);
     
     return 0;
 }
