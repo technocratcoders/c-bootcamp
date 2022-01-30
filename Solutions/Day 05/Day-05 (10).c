d5806982edb3dc897b6ecb958756b0217070f388
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-30 20:01:58 +0530

    Update Day-05 (10).c

--- Solutions/Day 05/Day-05 (10).c
+++ Solutions/Day 05/Day-05 (10).c
@@ -1,19 +1,25 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-05
+
+Q.10) Write a program to find the average of two numbers.
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
-    }
+    // Taking avg variable as float because it may contain decimal values
+    float avg;
     
-    printf("%d", ans);
+    // Calculating average
+    avg = (float) (num1 + num2) / 2;
+    
+    // Printing average
+    printf("The average of %d and %d is %.1f", num1, num2, avg);
     
     return 0;
 }
