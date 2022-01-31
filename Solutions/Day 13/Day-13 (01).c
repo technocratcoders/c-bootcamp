cb64484adff2cebc14e23ce8be6c83fb18864cb3
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-31 10:38:19 +0530

    Update Day-13 (01).c

--- Solutions/Day 13/Day-13 (01).c
+++ Solutions/Day 13/Day-13 (01).c
@@ -1,19 +1,46 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-13
+
+Q.1) Write a program to print the following pattern -
+* * * * 1 * * * *
+* * * 2 2 2 * * *
+* * 3 3 3 3 3 * *
+* 4 4 4 4 4 4 4 *
+5 5 5 5 5 5 5 5 5
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
+    int star = 0, count = 0;
+    
+    for(int i=1; i<=n; i++) {
+        
+        star = n - i;
+        // Printing the first triangle of stars
+        for(int j=1; j<=star; j++) {
+            printf("* ");
+        }
+        
+        count = (2 * i) - 1;
+        // Printing the first pyramid of numbers
+        for(int j=1; j<=count; j++) {
+            printf("%d ", i);
+        }
+        
+        star = n - i;
+        // Printing the second triangle of stars
+        for(int j=1; j<=star; j++) {
+            printf("* ");
+        }
+        
+        printf("\n");
     }
-    
-    printf("%d", ans);
     
     return 0;
 }
