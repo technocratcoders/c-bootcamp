bc50c470bb2d3c800b5a11c20e793572613ad228
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-31 10:44:24 +0530

    Update Day-10 Adv-(07).c

--- Solutions/Day 10/Advanced/Day-10 Adv-(07).c
+++ Solutions/Day 10/Advanced/Day-10 Adv-(07).c
@@ -2,7 +2,7 @@
 ---------- TECHNOCRAT CODERS ----------
 C-Language Training Bootcamp --> Day-10 (Advanced)
 
-Q.7) Write a program to print the following pattern -
+Q.7) Write a program to print the following pattern (for n=5) -
         1
       1 2 1
     1 2 3 2 1
