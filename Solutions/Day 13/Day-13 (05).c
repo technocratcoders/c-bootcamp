5acb2d77087f67a3a26718fb9d4fd35fab625ed4
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2022-01-31 11:25:51 +0530

    Update Day-13 (05).c

--- Solutions/Day 13/Day-13 (05).c
+++ Solutions/Day 13/Day-13 (05).c
@@ -1,19 +1,60 @@
+/*
+---------- TECHNOCRAT CODERS ----------
+C-Language Training Bootcamp --> Day-13
+
+Q.5) Write a program to print the following pattern (for n=5) -
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+*/
+
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int num;
-    scanf("%d", &num);
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
     
-    int rem, ans=0, i=0;
-    while(n) {
-        rem = n%2;
-        n/=2;
-        ans = (pow(10,i) * rem) + ans;          // reversing the bits
-        i++;
+    // Printing the upwards pyramid
+    for(int i=0; i<n; i++) {
+        
+        // Printing spaces
+        for(int j=0; j<=i; j++) {
+            printf(" ");
+        }
+        
+        int star = n - i;
+        // Printing the first triangle of stars
+        for(int j=1; j<=star; j++) {
+            printf("* ");
+        }
+        
+        printf("\n");
     }
     
-    printf("%d", ans);
+    // Printing the downwards pyramid
+    for(int i=1; i<=n; i++) {
+        
+        int space = n - i;
+        // Printing spaces
+        for(int j=0; j<=space; j++) {
+            printf(" ");
+        }
+        
+        // Printing the first triangle of stars
+        for(int j=1; j<=i; j++) {
+            printf("* ");
+        }
+        
+        printf("\n");
+    }
     
     return 0;
 }
